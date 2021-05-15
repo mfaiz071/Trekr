4bdf6e787d33760b9c746bec5a9926b24c72aae3
Mohammad Faizan <faizanadonis@hotmail.com>
2021-05-15 15:32:56 -0400

    Changes to add image to view

--- Trekr/ContentView.swift
+++ Trekr/ContentView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("GoodBye, world!")
-            .padding()
+        Image("highlands")
+            .resizable()
+            .scaledToFit()
     }
 }
 
