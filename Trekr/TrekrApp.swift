9da16e946801ef28bdcfccc6625c8fbc1f239256
Mohammad Faizan <faizanadonis@hotmail.com>
2021-05-15 15:24:24 -0400

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Mohammad Faizan on 2021-05-15.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
